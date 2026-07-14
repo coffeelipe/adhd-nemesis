bb32d77466c39b2b3abe3a92039c4e1cd3cba7cc
Felipe Pontes <fpontes321@gmail.com>
2026-07-13 23:06:13 -0300

    Add NavBar and Calendar widgets to the home page

--- app/lib/src/pages/home.dart
+++ app/lib/src/pages/home.dart
@@ -1,3 +1,6 @@
+import 'package:adhd_nemesis/src/core/extensions/context_extensions.dart';
+import 'package:adhd_nemesis/src/widgets/home/calendar.dart';
+import 'package:adhd_nemesis/src/widgets/home/month_selector.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -5,6 +8,38 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text("Hello World!")));
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.calendar_today_rounded),
+            label: "Home",
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.check_circle_outline),
+            label: "Habits",
+          ),
+          NavigationDestination(icon: Icon(Icons.alarm), label: "Tools"),
+        ],
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: context.padding,
+          child: Column(
+            children: [
+              const MonthSelector(),
+              ConstrainedBox(
+                constraints: BoxConstraints(
+                  maxHeight: MediaQuery.of(context).size.height * 0.3,
+                ),
+                child: const Calendar(),
+              ),
+              const SizedBox(height: 14),
+
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
