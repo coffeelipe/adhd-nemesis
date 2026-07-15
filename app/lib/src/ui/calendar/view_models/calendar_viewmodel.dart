1c03cf060b6f93640c8b163f4c9999e366230f1b
Felipe Pontes <fpontes321@gmail.com>
2026-07-15 20:30:08 -0300

    Implement the calendar viewmodel

---
+++ app/lib/src/ui/calendar/view_models/calendar_viewmodel.dart
@@ -0,0 +1,22 @@
+import 'package:adhd_nemesis/src/domain/models/calendar/calendar.dart';
+import 'package:adhd_nemesis/src/domain/models/calendar_event/calendar_event.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class CalendarViewModel extends ChangeNotifier {
+  final WidgetRef ref;
+
+  CalendarViewModel(this.ref);
+
+  Calendar get calendar => ref.watch(calendarProvider);
+
+  void createEvent(CalendarEvent event) {
+    ref.read(calendarProvider.notifier).createEvent(event);
+    notifyListeners();
+  }
+
+  void deleteEvent(int id) {
+    ref.read(calendarProvider.notifier).deleteEvent(id);
+    notifyListeners();
+  }
+}
